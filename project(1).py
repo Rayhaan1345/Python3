ddedc8596aa7333da4edf388cfe1d82fc14da518
Rayhaan1345 <96483570+Rayhaan1345@users.noreply.github.com>
2024-05-05 09:52:51 +0400

    Update project(1).py

--- project(1).py
+++ project(1).py
@@ -38,7 +38,7 @@ u = st.checkbox("Forecast")
 i = st.checkbox("Wind Speed 💨")
 def main():
     z = st.text_input("city: (please enter country code for places with same name in multiple regions eg: Cambridge, US)")
-    op = 5
+    op == 5
     if not z.startswith('a'):
         
         while op > 0:
